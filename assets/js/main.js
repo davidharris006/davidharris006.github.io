44f174d9e4004922d1ffb57ca2c0f2d13027bea0
David Harris <davidharris006@gmail.com>
2019-04-27 23:45:21 -0700

    final commits

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    $('#travelappbox').on('click', function () {
+        window.open('https://davidharris006.github.io/Project1')
+
+    })
+    $('#triviagamebox').on('click', function () {
+        window.open('https://davidharris006.github.io/TriviaGame/')
+
+    })
+    $('#crystalgamebox').on('click', function () {
+        window.open('https://davidharris006.github.io/unit-4-game/')
+
+    })
+    $('.fa-linkedin').on('click', function () {
+        window.open('https://www.linkedin.com/in/david-harris-634047127')
+    })
+    $('.fa-github-square').on('click', function () {
+        window.open('https://github.com/davidharris006')
+    })
+    $('.fa-file').on('click', function () {
+        window.open('https://documentcloud.adobe.com/link/track?uri=urn%3Aaaid%3Ascds%3AUS%3A8b382dbf-2ffd-47aa-b5dd-cf909da9e423   ')
+    })
+    $('#linkedinimg').on('click', function () {
+        window.open('https://www.linkedin.com/in/david-harris-634047127')
+    })
+    $('#gitHubimg').on('click', function () {
+        window.open('https://github.com/davidharris006')
+    })
+    $('#resumeimg').on('click', function () {
+        window.open('https://documentcloud.adobe.com/link/track?uri=urn%3Aaaid%3Ascds%3AUS%3A8b382dbf-2ffd-47aa-b5dd-cf909da9e423   ')
+    })
+
+
+});
